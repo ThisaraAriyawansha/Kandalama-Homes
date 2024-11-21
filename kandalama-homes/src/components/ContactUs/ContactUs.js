633e71eed2182a1e756d793d1f56d637eb55d1de
ThisaraAriyawansha <thisara.a2001@gmail.com>
2024-11-21 15:19:02 +0530

    update

---
+++ kandalama-homes/src/components/ContactUs/ContactUs.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Navbar from "../NavBar";
+import Footer from "../Footer";
+import ContactUs_1 from "./ContactUs_1";
+import AboutUs_4 from "../AboutUs/AboutUs_4";
+
+
+
+const AboutUs = () => {
+  return (
+    <div>
+      <Navbar />
+    <ContactUs_1/>
+    <AboutUs_4/>
+    <Footer/>
+
+    </div>
+  );
+};
+
+export default AboutUs;
