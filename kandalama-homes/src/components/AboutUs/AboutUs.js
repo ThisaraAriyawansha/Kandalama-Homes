ce64c81569082dff071a9a46403b5965ce3b0828
ThisaraAriyawansha <thisara.a2001@gmail.com>
2024-11-21 13:43:08 +0530

    update

--- kandalama-homes/src/components/AboutUs/AboutUs.js
+++ kandalama-homes/src/components/AboutUs/AboutUs.js
@@ -2,7 +2,8 @@ import React from "react";
 import Navbar from "../NavBar";
 import Footer from "../Footer";
 import AboutUs_1 from "./AboutUs_1";
-
+import AboutUs_2 from "./AboutUs_2";
+import AboutUs_3 from "./AboutUs_3";
 
 
 const AboutUs = () => {
@@ -10,6 +11,8 @@ const AboutUs = () => {
     <div>
       <Navbar />
     <AboutUs_1/>
+    <AboutUs_2/>
+    <AboutUs_3/>
     <Footer/>
 
     </div>
