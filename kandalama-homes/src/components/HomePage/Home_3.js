89c150b55f7cac03b3034c53418772d123110c0b
ThisaraAriyawansha <thisara.a2001@gmail.com>
2024-11-26 10:19:39 +0530

    update

--- kandalama-homes/src/components/HomePage/Home_3.js
+++ kandalama-homes/src/components/HomePage/Home_3.js
@@ -42,7 +42,7 @@ const Video = () => {
   }, []);
 
   const videoUrl =
-    "https://s3-figma-videos-production-sig.figma.com/video/1337030861483380104/TEAM/3993/4a10/-9d79-4e22-a4eb-9c980600b937?Expires=1733097600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=gYU8dd7AZUzPXveHTtRSJJ3ja0IprHzgnwDCd1rSDORYQHosyXIbKX~fCrDuIJZ-id~cWFNGVOMGfCkp6Q1x1hpYmLmymjPzsjvBhmqdCf6uDmniMtkS-4mXyCgpcuYCQ01uIYigEB91wRbPUtwKGxY5A98kQNdcmVWzQSSr7igXvE2HcY8iHbJbOBsDTrXUwrY9yCre46W-fSm742mKBuLY1cayKH8XpqA2BAKjcspRJm~iamMdVRID-IMlWML-ix3uJ1dgzDNk4l8sUt~on2pBlgOe79ctvCXBmZR-0so-n2XPIrnJmhPRR0EqplRvV~U8EJX1-RWY85tVi1cGWQ__"; // Replace with your video URL
+    "/images/-9d79-4e22-a4eb-9c980600b937.mp4"; // Replace with your video URL
 
   return (
     <div className="mt-10 mb-20 sm:mb-10">
